1f01885d191a1a72c2bbc51129411044029e2854
BANG BAE <peardanny@gmail.com>
2022-06-16 04:13:48 +0900

    1st commit

---
+++ project/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+	let [title,b] = useState('recommendation')
+
+
+	return (
+	<div className="App">
+    	<div className = "black-nav">
+    		<h1 style = {{color: "white"}}>더 비쌀까 쌀까?</h1>
+    	</div>
+    	<div className= "game-screen" style={{background:"dimgray"}}>
+        	
+        	<div className= "Left-box">
+				{/* <img src={require('./img/42e863669e2698303171baa9c2467987.jpg').default}  /> */}
+				<img src="img/42e863669e2698303171baa9c2467987.jpg" class= "img-thumbnail"/>
+			</div>
+			<div className= 'Right-box'>
+				<button class = "Expensive-button" type = "button" onClick={console.log("expensive!")}>비싸다!</button>
+				<button class = "Cheap-button" type = "button" onClick={console.log("expensive!")}>싸다!</button>
+				<img src="img/39876858ec6a3109c42e71b37504983e.jpg" class = "img-thumbnail"/>
+			</div>
+    	</div>
+    </div>
+    );
+}
+
+export default App;
